d2385a6fa204e06c57eee41d6b78b65c61e25c5a
RanaMagica <angelm1643@gmail.com>
2024-04-15 03:07:44 +0200

    control de versiones
    test

---
+++ Assets/course/12 control de versiones/controlTest.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace course
+{
+    public class controlTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            System.Console.WriteLine("test");
+        }
+    }
+}
